46a28e1c2236659fb01e73b6fa001d7007637c2c
Tony <aj.crowther@hotmail.com>
2018-05-09 09:48:09 +0100

    updated core

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task ('watch', function(){
+gulp.task('watch', function() {
+
   browserSync.init({
     notify: false,
     server: {
@@ -10,16 +11,17 @@ gulp.task ('watch', function(){
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', function(){
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
 
 });
-gulp.task('cssInject', ['styles'], function(){
-  return gulp.src('./app/temp/styles/sytle.css')
-  .pipe(browserSync.stream());
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
